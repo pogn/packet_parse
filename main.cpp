14f728f9d2cfe2f6115016046f76765575382ec2
yuio1213 <ujungin1213@gmail.com>
2017-07-24 09:17:04 -0400

    update source code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -20,25 +21,25 @@
 
 /* Ethernet header */
 struct ethernet_header{
-        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
-        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
-        u_short ether_type;                     /* IP? ARP? RARP? etc */
+        u_int8_t  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_int8_t  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        uint16_t ether_type;                     /* IP? ARP? RARP? etc */
 };
 
 /* IP header */
 struct ip_header{
-        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
-        u_char  ip_tos;                 /* type of service */
-        u_short ip_len;                 /* total length */
-        u_short ip_id;                  /* identification */
-        u_short ip_off;                 /* fragment offset field */
+        u_int8_t ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_int8_t ip_tos;                 /* type of service */
+        uint16_t ip_len;                 /* total length */
+        uint16_t ip_id;                  /* identification */
+        uint16_t ip_off;                 /* fragment offset field */
         #define IP_RF 0x8000            /* reserved fragment flag */
         #define IP_DF 0x4000            /* dont fragment flag */
         #define IP_MF 0x2000            /* more fragments flag */
         #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-        u_char  ip_ttl;                 /* time to live */
-        u_char  ip_p;                   /* protocol */
-        u_short ip_sum;                 /* checksum */
+        u_int8_t  ip_ttl;                 /* time to live */
+        u_int8_t  ip_p;                   /* protocol */
+        uint16_t ip_sum;                 /* checksum */
         struct  in_addr ip_src,ip_dst;  /* source and dest address */
 };
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
@@ -48,13 +49,13 @@ struct ip_header{
 typedef u_int tcp_seq;
 
 struct tcp_header {
-        u_short th_sport;               /* source port */
-        u_short th_dport;               /* destination port */
+        uint16_t th_sport;               /* source port */
+        uint16_t th_dport;               /* destination port */
         tcp_seq th_seq;                 /* sequence number */
         tcp_seq th_ack;                 /* acknowledgement number */
-        u_char  th_offx2;               /* data offset, rsvd */
+        u_int8_t  th_offx2;               /* data offset, rsvd */
 #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
-        u_char  th_flags;
+        u_int8_t  th_flags;
         #define TH_FIN  0x01
         #define TH_SYN  0x02
         #define TH_RST  0x04
@@ -64,9 +65,9 @@ struct tcp_header {
         #define TH_ECE  0x40
         #define TH_CWR  0x80
         #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 /* window */
-        u_short th_sum;                 /* checksum */
-        u_short th_urp;                 /* urgent pointer */
+        uint16_t th_win;                 /* window */
+        uint16_t th_sum;                 /* checksum */
+        uint16_t th_urp;                 /* urgent pointer */
 };
 
 int main(int argc, char *argv[])
@@ -80,32 +81,40 @@ int main(int argc, char *argv[])
        bpf_u_int32 mask;		/* Our netmask */
        bpf_u_int32 net;		/* Our IP */
        struct pcap_pkthdr *header;	/* The header that pcap gives us */
-       const u_char *packet;	/* The actual packet */
+       const u_int8_t *packet;	/* The actual packet */
        int res,i,k,num;
        struct ethernet_header *pEth;
        struct ip_header *pIph;
        struct tcp_header *pTcp;
+       int len;
 
+       /* Define the device */
+       //dev = pcap_lookupdev(errbuf);
+       //if (dev == NULL) {
+       //    fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+       //    return(2);
+       //}
 
-       /* Define the evice */
-       dev = pcap_lookupdev(errbuf);
-       if (dev == NULL) {
-           fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-           return(2);
-       }
        /* Find the properties for the device */
+       if (!argv[1]){
+           fprintf(stderr,"Please input the device name");
+       }
+       dev=argv[1];
        if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
            fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
            net = 0;
            mask = 0;
        }
+
        /* Open the session in promiscuous mode */
-       handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+       handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf); //dev --> "dum0"
        if (handle == NULL) {
            fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
            return(2);
        }
-       /* Compile and apply the filter */
+
+       /* make error when the port num is not 80
+       // Compile and apply the filter
        if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
            fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
            return(2);
@@ -114,13 +123,15 @@ int main(int argc, char *argv[])
            fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
            return(2);
        }
+       */
 
-       /* Grab a packet */
+/* Grab a packet */
        num=0;
        printf("-----------grabbing 10 packets------------------\n");
        printf("grabbing 10 packets\n");
+
        while((res = pcap_next_ex(handle, &header, &packet))>=0){ //double pointer
-           if(res==0) {continue; }
+           if(res==0) {continue;}
            num++;
            if(num ==10)
                break;
@@ -134,9 +145,17 @@ int main(int argc, char *argv[])
        pIph = (struct ip_header *)(packet + sizeof(*pEth));
        pTcp = (struct tcp_header *)(packet + sizeof(*pEth) + sizeof(*pIph));
 
-       //printf("packet : %s\n", pEth->ether_dhost);
+       /* check IP and TCP and port num */
+       if(pEth->ether_type!=0x0008){
+           fprintf(stderr, "-----------------------This is not IP packet----------------------");
+           continue;
+       }
+       if(pIph->ip_p != 0x06){
+           fprintf(stderr, "-----------------------This is not TCP packet---------------------");
+           continue;
+       }
 
-       // print ehternet
+       /* print ehternet */
        fprintf(stdout, "DESTINATION MAC Address - [");
        for( i = 0 ; i < 6 ; ++i)
        {
@@ -151,17 +170,21 @@ int main(int argc, char *argv[])
        }
        fprintf(stdout, "\b]\n");
 
-       // print IP
-       fprintf(stdout, "SOURCE IP address       - [%s]\n", inet_ntoa(pIph->ip_src));
-       fprintf(stdout, "DESTINATION IP address  - [%s]\n", inet_ntoa(pIph->ip_dst));
+// print IP
+       char tmp[INET_ADDRSTRLEN];
+       inet_ntop(AF_INET, &(pIph->ip_src), tmp, INET_ADDRSTRLEN);
+       fprintf(stdout, "SOURCE IP address       - [%s]\n", tmp); //inet_ntoa(pIph->ip_src));
+       inet_ntop(AF_INET, &(pIph->ip_dst), tmp, INET_ADDRSTRLEN);
+       fprintf(stdout, "DESTINATION IP address  - [%s]\n", tmp);//inet_ntoa(pIph->ip_dst));
 
-       // print TCP
+// print TCP
        fprintf(stdout, "SOURCE port             - [%hu]\n", ntohs(pTcp->th_sport)); //recommandation - befor using %hu, print "%02x" to see real hex value
        fprintf(stdout, "DESTINATION port        - [%hu]\n", ntohs(pTcp->th_dport)); //without ntohs, it will read memory "little endian"
 
-       // print Data
+// print Data
        fprintf(stdout,"DATA\n");
-       for(i = sizeof(*pEth) + sizeof(*pIph) + sizeof(*pTcp) ; i < header->len ; i+=16)
+       len=ntohs(pIph ->ip_len);
+       for(i = sizeof(*pEth) + sizeof(*pIph) + sizeof(*pTcp) ; i < len ; i+=16)
        {
           fprintf(stdout, "[%08x] ", i-(sizeof(*pEth) + sizeof(*pIph) + sizeof(*pTcp)));
           for(k = 0 ; k < 16  ; ++k)
